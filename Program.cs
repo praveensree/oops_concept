33b45f6c32a94c52ffa7d23430785805d41e61fb
Praveen P <praveen.padmanabhan@bhavnacorp.com>
2022-02-03 20:04:26 +0530

    first commit

---
+++ Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+namespace ClassObjectsDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee e1 = new Employee(752,23,"TamilNadu", "Praveen");
+            Employee e2 = new Employee(754, 24, "Hyderabad", "Anudeep");
+            e1.Display();
+            e2.Display();
+
+            Console.WriteLine("Enter the first value");
+            int first = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the Second value");
+            int second = Convert.ToInt32(Console.ReadLine());
+
+
+
+            e1.Sum(first, second);
+
+        }
+    }
+
+
+
+    class Employee
+    {
+        int eid, eage;
+        string eaddress, ename;
+        public Employee(int id, int age, string address, string name)
+        {
+            eid = id;
+            eage = age;
+            ename = name;
+            eaddress = address;
+        }
+        public void Display()
+        {
+            Console.WriteLine($"employee id is:  { eid}");
+            Console.WriteLine($"employee name is:  { ename}");
+            Console.WriteLine($"employee age is:  { eage}");
+            Console.WriteLine($"employee address is: { eaddress}");
+            Console.WriteLine(" ");
+        }
+        public void Sum(int no1, int no2)
+        {
+            int res = no1 + no2;
+            Console.WriteLine($"The value is  {res}");
+        }
+    }
+
+}
+
+
+
